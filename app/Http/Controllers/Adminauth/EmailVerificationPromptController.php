ca11670a7dcbd23691ece7fac2b2dd7208cab9ca
baronlegacy256 <aaronbaraka292@gmail.com>
2023-05-27 01:55:26 +0300

    implemented the multi auth system

---
+++ app/Http/Controllers/Adminauth/EmailVerificationPromptController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Adminauth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class EmailVerificationPromptController extends Controller
+{
+    /**
+     * Display the email verification prompt.
+     */
+    public function __invoke(Request $request): RedirectResponse|Response
+    {
+        return $request->user()->hasVerifiedEmail()
+            ? redirect()->intended(RouteServiceProvider::ADMIN_HOME)
+            : Inertia::render('Auth/VerifyEmail', ['status' => session('status')]);
+    }
+}
